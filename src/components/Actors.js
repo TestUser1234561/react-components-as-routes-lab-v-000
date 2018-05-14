e994bdf9064e4029f8a52cdc9b73139cba5dd6f3
Morgan Fudge <mfudge92@gmail.com>
2018-05-14 03:52:07 +0000

    automatically backed up by learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -6,15 +6,15 @@ const Actors = () => {
     <div>
       <h1>Actors</h1>
       {actors.map((actor, index) => (
-          <div key={index}>
-              <h3>Name: {actor.name}</h3>
-              <p>Movies:</p>
-              <ul>
-                  {actor.movies.map((movie, index) => (
-                      <li key={index}>{movie}</li>
-                  ))}
-              </ul>
-          </div>
+        <div key={index}>
+          <h3>Name: {actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
       ))}
     </div>
   );
